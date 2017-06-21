fb0fc530ce0370b4c16e29c4da9bbeeca9cd42b3
Samer Kadamani <samerk@fb.com>
2017-06-21 16:17:58 -0700

    Initial commit

---
+++ app/src/main/java/com/codepath/flixster/MovieListActivity.java
@@ -0,0 +1,131 @@
+package com.codepath.flixster;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.codepath.flixster.models.Movie;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MovieListActivity extends AppCompatActivity {
+
+    // constants
+    // the base url for the API
+    public static final String API_BASE_URL = "http://api.themoviedb.org/3";
+    // parameter name for the API key
+    public final static String API_KEY_PARAM = "api_key";
+    // tag for logging from this activity
+    public final static String TAG = "MovieListActivity";
+
+    // instance fields
+    AsyncHttpClient client;
+    // the base url for loading images
+    String imageBaseUrl;
+    // the poster size to use when fetching images
+    String posterSize;
+    // the list of currently playing movies
+    ArrayList<Movie> movies;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_list);
+        // initiate the client
+        client = new AsyncHttpClient();
+        // initialize the list of movies
+        movies = new ArrayList<>();
+        // get the configuration on app creation
+        getConfiguration();
+    }
+
+    // get the list of currently playing movies
+    private void getNowPlaying(){
+        // create the url
+        String url = API_BASE_URL + "/movie/now_playing";
+        // set the request parameters
+        RequestParams params = new RequestParams();
+        params.put(API_KEY_PARAM, getString(R.string.api_key)); // API key (always required)
+        // execute a GET request expecting a JSON object response
+        client.get(url, params, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // load the results into movies List
+                try {
+                    JSONArray results = response.getJSONArray("results");
+                    for (int i = 0; i < results.length(); i++){
+                        Movie movie = new Movie(results.getJSONObject(i));
+                        movies.add(movie);
+                    }
+                    Log.i(TAG, String.format("Loaded %s movies", results.length()));
+
+                } catch (JSONException e) {
+                    logError("failed to parse response data", e, true);
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                logError("Failed to get now playing data", throwable, true);
+            }
+        });
+    }
+
+    // get the configuration from the API
+    private void getConfiguration(){
+        // create the url
+        String url = API_BASE_URL + "/configuration";
+        // set the request parameters
+        RequestParams params = new RequestParams();
+        params.put(API_KEY_PARAM, getString(R.string.api_key)); // API key (always required)
+        // execute a GET request expecting a JSON object response
+        client.get(url, params, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    JSONObject images = response.getJSONObject("images");
+                    imageBaseUrl = images.getString("secure_base_url");
+                    // get the poster size
+                    JSONArray posterSizeOptions = images.getJSONArray("poster_sizes");
+                    // use option 3 in the array
+                    posterSize = posterSizeOptions.optString(3, "w342");
+
+                    Log.i(TAG, String.format("Loaded configuration with imageBaseUrl %s and " +
+                            "posterSize %s", imageBaseUrl, posterSize));
+                    // get the now playing movie list
+                    getNowPlaying();
+                } catch (JSONException e) {
+                    logError("failed to parse configuration", e, true);
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                logError("Failed to get configuration", throwable, true);
+            }
+        });
+
+
+    }
+
+    // handle errors; log and alert user
+    private void logError(String message, Throwable error, boolean alertUser){
+        Log.e(TAG, message, error);
+        // alert the user to avoid silent errors
+        if (alertUser){
+            // show a long Toast with the error message
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+
+        }
+    }
+}
